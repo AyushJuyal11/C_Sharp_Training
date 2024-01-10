174d13a0ee48dfdca2fde2972bafea47094d487e
ayush <ayush.juyal@remotestate.com>
2024-01-10 17:00:26 +0530

    added services folder and modified the controller

--- StudentApplication/StudentApplication/DAL/Entities/Student.cs
+++ StudentApplication/StudentApplication/DAL/Entities/Student.cs
@@ -2,18 +2,10 @@
 {
     public class Student
     {
-        public string Name {  get; set; }
-        public int Id { get; set; }
+        public string? Name {  get; set; }
+        public int? Id { get; set; }
         public int Age {  get; set; }
         public int Marks { get; set; }
-
-        public Student(string name, int id, int age, int marks)
-        {
-            Name = name; 
-            Id = id;
-            Age = age;
-            Marks = marks;
-        }
-
+        
     }
 }
