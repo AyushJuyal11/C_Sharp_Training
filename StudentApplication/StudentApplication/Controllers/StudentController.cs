ac0fac16e3f9dac39e5de583da755a27679be560
ayush <ayush.juyal@remotestate.com>
2024-01-10 13:29:50 +0530

    initial commit student crud app

---
+++ StudentApplication/StudentApplication/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApplication.DAL.Entities;
+using System.Linq;
+
+namespace StudentApplication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+
+        List<Student> students = new()
+        {
+            new Student("ayush", 1, 23, 69),
+            new Student("someone", 2, 24, 99)
+        };
+
+        [HttpGet("get-students")]
+        public ActionResult<List<Student>> GetStudents()
+        {
+            return students;
+        }
+
+        [HttpPost("add-student")]
+        public ActionResult<List<Student>> AddStudent([FromBody]Student student)  
+        {
+            students.Add(new Student(student.Name, student.Id, student.Age, student.Marks));
+            return students;
+        }
+
+        [HttpDelete("delete-student")]
+        public ActionResult<List<Student>> DeleteStudent(int id)
+        {
+
+            students.RemoveAll(stu => stu.Id == id);
+            return students; 
+        }
+
+        [HttpPut("update-student-details")]
+        public ActionResult<List<Student>> UpdateStudentDetails([FromBody]Student student) 
+        {
+            Student stu = students.FirstOrDefault(stu => stu.Id == student.Id);
+
+            if(stu != null)
+            {
+                stu.Name = student.Name;
+                stu.Age = student.Age;
+                stu.Marks = student.Marks;
+                stu.Id = student.Id;
+            }
+            return students; 
+        }
+    }
+}
---
+++ StudentApplication/StudentApplication/DAL/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentApplication.DAL.Entities
+{
+    public class Student
+    {
+        public string Name {  get; set; }
+        public int Id { get; set; }
+        public int Age {  get; set; }
+        public int Marks { get; set; }
+
+        public Student(string name, int id, int age, int marks)
+        {
+            Name = name; 
+            Id = id;
+            Age = age;
+            Marks = marks;
+        }
+
+    }
+}
